cdff2adb2e2ad408982ddd99fec85dd282f301de
Dawid Kostrzewa <dawid.ko1997@gmail.com>
2022-12-23 13:55:55 +0100

    improve: seo & accesibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,12 @@ import '../styles/globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html className='dark'>
+        <html className='dark' lang='en'>
             <head>
                 <title>Dawid Kostrzewa - Software Engineer</title>
                 <link rel="icon" href="/favicon.ico" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="I am a software engineer based in Kraków, Poland. I am working on web applications including the newest frameworks and libraries such as Reach, Angular, TypeScript, NodeJS. I like to learn new things and I am looking forward to working with great people." />
             </head>
             <body>
                 <div className="bg-white dark:bg-black dark:text-white">
