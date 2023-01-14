9627ce001e6ee6200eb23894184f1321a688a101
Dawid Kostrzewa <dawid.ko1997@gmail.com>
2023-01-14 21:09:55 +0100

    feat: add favicon

--- app/head.tsx
+++ app/head.tsx
@@ -9,7 +9,12 @@ export default function Head() {
                 content="I am a software engineer based in Kraków, Poland. I am working on web applications including the newest frameworks and libraries such as Reach, Angular, TypeScript, NodeJS. I like to learn new things and I am looking forward to working with great people."
             />
             <title>Dawid Kostrzewa - Software Engineer</title>
+
             <link rel="icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
 
             <Script
                 strategy="lazyOnload"
