4f44df4f700f5c8abe72a59c9b60f6c57f119f83
Dawid Kostrzewa <dawid.ko1997@gmail.com>
2023-01-15 15:36:48 +0100

    fix: GA

--- app/blog/[slug]/head.tsx
+++ app/blog/[slug]/head.tsx
@@ -1,4 +1,5 @@
 import { BlogFields } from '../../../components/Blog/Blog.types';
+import { GAScript } from '../../../components/GAScript/GAScript';
 import { client } from '../../../contentful/client';
 
 const getEntryTitle = async (slug: string) => {
@@ -22,6 +23,7 @@ export default async function Head({ params }: { params: { slug: string } }) {
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             <link rel="manifest" href="/site.webmanifest" />
+            <GAScript />
         </>
     );
 }
